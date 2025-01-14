1ea857f096536f3d8b3846438e01e00d159c1373
phenomonox <me@phenomonox.com>
2025-01-14 02:01:39 -0500

    adding functions get_book_text, get_word_count, and get_letter_count

---
+++ main.py
@@ -0,0 +1,56 @@
+def get_letter_count(contents):
+    letter_count = {
+        "a": 0,
+        "b": 0,
+        "c": 0,
+        "d": 0,
+        "e": 0,
+        "f": 0,
+        "g": 0,
+        "h": 0,
+        "i": 0,
+        "j": 0,
+        "k": 0,
+        "l": 0,
+        "m": 0,
+        "n": 0,
+        "o": 0,
+        "p": 0,
+        "q": 0,
+        "r": 0,
+        "s": 0,
+        "t": 0,
+        "u": 0,
+        "v": 0,
+        "w": 0,
+        "x": 0,
+        "y": 0,
+        "z": 0
+    }
+    all_letters = list(contents)
+    
+    for letter in all_letters:
+        letter = letter.lower()
+        if letter in letter_count:
+            letter_count[letter] = letter_count[letter]+1
+
+    return letter_count
+
+
+def get_word_count(contents):
+    words = contents.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = get_word_count(text)
+    letter_count = get_letter_count(text)
+    print(letter_count)
+
+main()
